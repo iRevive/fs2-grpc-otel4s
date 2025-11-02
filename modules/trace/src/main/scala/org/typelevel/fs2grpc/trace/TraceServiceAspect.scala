90caf77f1c4d094722eaca70fd4e0bdded759ec1
Maksym Ochenashko <maxochenashko@gmail.com>
2025-11-02 13:45:58 +0200

    Create headers

--- modules/trace/src/main/scala/org/typelevel/fs2grpc/trace/TraceServiceAspect.scala
+++ modules/trace/src/main/scala/org/typelevel/fs2grpc/trace/TraceServiceAspect.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.typelevel.fs2grpc.trace
 
 import cats.effect.MonadCancelThrow
@@ -150,6 +166,26 @@ object TraceServiceAspect {
           finalizationStrategy: (AspectOperation, ServiceCallContext[?, ?]) => SpanFinalizer.Strategy
       ): Config =
         copy(finalizationStrategy = finalizationStrategy)
+    }
+
+  }
+
+  def create[F[_]: MonadCancelThrow: TracerProvider]: F[ServiceAspect[F, F, Metadata]] =
+    create(Config.default)
+
+  def create[F[_]: MonadCancelThrow: TracerProvider](config: Config): F[ServiceAspect[F, F, Metadata]] =
+    TracerProvider[F].tracer(config.tracerName).withVersion(BuildInfo.version).get.map { implicit tracer =>
+      implicit val textMapGetter: TextMapGetter[Metadata] = config.textMapGetter
+
+      new TraceServiceAspect[F](
+        config.spanName,
+        config.attributes,
+        config.finalizationStrategy,
+      )
+    }
+
+}
+rategy = finalizationStrategy)
     }
 
   }
